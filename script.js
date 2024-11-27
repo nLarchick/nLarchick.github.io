94d1148cca1eeaeea60407f5d30473f2203c446f
Noah <larchicknoah@gmail.com>
2024-11-27 13:42:41 -0700

    Added javscript file, as well as linked it to HTML file

---
+++ script.js
@@ -0,0 +1,8 @@
+/**
+ * File: script.js
+ * Description: Javascript for managing dynamic Single
+ *  page application.
+ * Author: Noah Larchick
+ * Date (created): 11/27/2024
+ * Date (curr): 11/27/2024
+ */
