1ed660ee68a1eaef4981e08208ccf09fd1fc3193
Noah <larchicknoah@gmail.com>
2024-12-02 13:17:26 -0700

    Removed bad content component.

--- script.js
+++ script.js
@@ -6,3 +6,20 @@
  * Date (created): 11/27/2024
  * Date (curr): 11/27/2024
  */
+
+document.addEventListener("DOMContentLoaded", () => {
+    const header = document.querySelector(".heading");
+
+    const handleScroll = () => {
+        if (window.scrollY < 50) {
+            header.classList.add("transparent");
+        } else {
+            header.classList.remove("transparent");
+        }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    // Run the function on page load
+    handleScroll();
+});
